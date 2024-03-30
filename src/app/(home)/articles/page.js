fbac6c066379b6ba3e41e3fc529592cf2e3ec45d
abidur-abid <abidur.abid40@gmail.com>
2024-03-30 20:22:14 +0600

    updated code

--- src/app/(home)/articles/page.js
+++ src/app/(home)/articles/page.js
@@ -1,3 +1,4 @@
+
 'use client'
 import { useEffect, useState } from 'react';
 import SectionTitle from "@/app/shared components/SectionTitle";
@@ -9,7 +10,8 @@ import toast from 'react-hot-toast';
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const Articles = () => {
-    const [posts, setPosts] = useState([]);
+  
+    const [posts, setPosts] = useState(null);
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -33,28 +35,39 @@ const Articles = () => {
         fetchPosts();
     }, []); // Fetch data on component mount
 
+    const postsArray = posts?.posts;
+
+    console.log(postsArray);
+
+ 
+
     return (
         <section>
             <div className="lg:-mt-20 -mt-0">
                 <SectionTitle title={'Articles'}/>
             </div>
-            <div className="grid lg:grid-cols-3 justify-center items-center grid-cols-1  gap-5  mb-20">
-                {posts.map((p, index) => (
-                    <div className="card bg-headerFooterColor shadow-xl image-full mx-auto" key={p._id}>
-                        <figure><Image src={articlebg} alt="Article Background" /></figure>
-                        <div className="card-body">
-                            <p className="text-xl font-bold text-headerFooterColor">{p.topicName}</p>
-                            <p className="text-md font-semibold text-headerFooterColor">Course Name: {p.courseName}.</p>
-                            <p className="text-md text-headerFooterColor">Date: {p.date}</p>
+            <div className="grid lg:grid-cols-3 justify-center items-center grid-cols-1 gap-5 mb-20">
+                { postsArray &&
+                    postsArray.map((p, index) => (
+                        <div className="card bg-headerFooterColor shadow-xl image-full mx-auto" key={p._id}>
+                            <figure>
+                                <Image src={articlebg} alt="Article Background" width={300} height={200} className='bg-cover' />
+                            </figure>
+                            <div className="card-body">
+                                <p className="text-xl font-bold text-headerFooterColor">{p.topicName}</p>
+                                <p className="text-md font-semibold text-headerFooterColor">Course Name: {p.courseName}.</p>
+                                <p className="text-md text-headerFooterColor">Date: {p.date}</p>
 
-                            <div className="card-actions justify-end">
-                                <Link href={`/articles/${p._id}`}>
-                                    <button className="button text-gray-600 hover:text-headerFooterColor">View Details</button>
-                                </Link>
+                                <div className="card-actions justify-end">
+                                    <Link href={`/articles/${p._id}`}>
+                                        <button className="button text-gray-600 hover:text-headerFooterColor">View Details</button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    )
+               
+                )}
             </div>
         </section>
     );
